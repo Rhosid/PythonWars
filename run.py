4681ec965912c02ff62ea1c4aac6c23ad6abfbb0
Spencer <rhosid206@gmail.com>
2014-11-24 18:32:56 -0600

    Created run.py

---
+++ run.py
@@ -0,0 +1,11 @@
+"""
+Name: run.py
+Description: menu holding all programs in Programs folder
+Version: 1.0.0
+Python: 3.3.5
+"""
+
+__author__ = "Spencer Dockham"
+__date__ = "11/24/2014"
+
+# not started yet..
