8f350e64566d1acf6af5cbe5b00454552d09876f
Spencer <rhosid206@gmail.com>
2014-11-24 18:19:54 -0600

    Created acronymGenerator.py

---
+++ Projects/acronymGenerator.py
@@ -0,0 +1,32 @@
+"""
+Name: acronymGenerator.py
+Description: creates acronyms out of a string of words
+Version: 1.0.0
+"""
+
+__author__ = "Spencer Dockham"
+__date__ = "10/29/2014"
+
+# DEF
+
+def acroGen(string):
+    string = string.title()
+    acro = ""
+    for ch in string:
+        if ch in capabet:
+            acro += ch
+    return acro
+
+# LISTS
+
+capabet = ['A','B','C','D','E','F','G','H','I','J',
+           'K','L','M','N','O','P','Q','R','S','T',
+           'U','V','W','X','Y','Z']
+
+# MAIN
+
+# acronym generator
+print("acronymGenerator.py")
+string = input("Please Enter String: ")
+acro = acroGen(string)
+print("Acronym: "+acro)
