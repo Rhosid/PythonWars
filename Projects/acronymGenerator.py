2a9c73273c7d041793d44328b6ae1f104f3ed13a
Spencer <rhosid206@gmail.com>
2014-11-27 15:39:39 -0600

    Updated acronymGenerator.py
    acronymGenerator is fully prepared to be ran through github

--- Projects/acronymGenerator.py
+++ Projects/acronymGenerator.py
@@ -1,8 +1,12 @@
 """
 Name: acronymGenerator.py
-Stuffs: by Spencer and Nick!
-Date: 10/29/14
+Description: generates an acronym out of a sentence
+Version: 1.0.1
+Python: 3.3.5
 """
+
+__author__ = "Spencer Dockham"
+__date__ = "10/20/2014"
 
 # DEF
 
@@ -27,3 +31,8 @@ print("acronymGenerator.py")
 string = input("Please Enter String: ")
 acro = acroGen(string)
 print("Acronym: "+acro)
+
+# program concluded
+print("Done")
+# pause keeps the command window open
+pause = input("Press any key to end: ")
